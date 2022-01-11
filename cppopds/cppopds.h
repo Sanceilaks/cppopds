65b40db9a16a6008388157f68fa0358bf5a073a2
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2022-01-11 15:06:29 +0300

    Update cppopds.h

--- cppopds/cppopds.h
+++ cppopds/cppopds.h
@@ -1,18 +1,48 @@
 ﻿#pragma once
 
 #include <string_view>
-#include <filesystem>
-#include <fstream>
+#include <optional>
+#include <vector>
 
 namespace cppopds {
+	using default_content_t = std::string;
+
 	struct author_t {
 		std::string name;
 		std::string uri;
 	};
 
+	struct category_t {
+		std::string scheme;
+		std::string term;
+		std::string label;
+	};
+
+	struct summary_t {
+		std::string type;
+		default_content_t summary;
+	};
+
+	struct content_t {
+		std::string type;
+		default_content_t content;
+	};
+
+	struct link_t {
+		std::string rel;
+		std::string href;
+		std::string type;
+	};
+
 	struct entry_t {
 		std::string title;
 		std::string id;
+
+		std::optional<std::string> updated;
+		std::optional<author_t> author;
+		std::optional<summary_t> summary;
+		std::optional<content_t> content;
+		std::optional<std::vector<link_t>> links;
 	};
 
 	struct opds_document {
